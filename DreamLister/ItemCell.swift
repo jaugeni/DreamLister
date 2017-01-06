5ff5680c364842a1a21f1ff9df213c194cbda8c4
jaugeni <jaugeni@gmail.com>
2017-01-06 18:55:07 -0500

    NSFetch set up

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  itemCell.swift
+//  DreamLister
+//
+//  Created by YAUHENI IVANIUK on 1/6/17.
+//  Copyright © 2017 YauheniIvaniuk. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
